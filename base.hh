d3b3edd661111d1a3968242c78ecfc882e5860bc
mcstar <liquid.phynix@gmail.com>
2011-10-21 22:09:59 +0200

    "first commit"

--- base.hh
+++ base.hh
@@ -15,15 +15,26 @@ struct inputSnippet{
   int col;
   int owner;
 
-  inputSnippet(std::string oKind, int rRow, int cCol, int oOwner) : kind(oKind[0]), row(rRow), col(cCol), owner(oOwner){
+  inputSnippet(std::string oKind, int rRow, int cCol, int oOwner):
+    kind(oKind[0]), row(rRow), col(cCol), owner(oOwner){
   };
+};
+
+struct s_pair{
+  int row;
+  int col;
+  s_pair(int rRow, int cCol):
+    row(rRow), col(cCol){};
 };
 
 
 struct s_gameData{
-  array<int> board;
-
-  s_gameData(int row, int col) : board(row, col){};
+  array<int> objBoard;
+  array<float> board;
+  array<float> tmpBoard;
+  vector<s_pair> water;
+  s_gameData(int row, int col):
+    objBoard(row, col), board(row, col), tmpBoard(row, col), water(){};
 };
 
 
@@ -31,6 +42,14 @@ typedef std::map<std::string, int> t_paramMap;
 typedef std::vector<inputSnippet> t_inputVector;
 
 void go(void);
-s_gameData* initGame(int rows, int cols);
-void outputDirections(t_inputVector&);
+s_gameData initGame(int, int);
+void outputDirections(s_gameData&, t_inputVector&);
 char randomDirection(void);
+void setInfluence(s_gameData&, t_inputVector&);
+void diffuseInfluence(s_gameData&, t_inputVector&);
+int wrap(int, int);
+char positionToDirection(int, int, int, int);
+char getMinValDirection(int, int, s_gameData&);
+void processWater(s_gameData&, inputSnippet&);
+void floatToRGB(float&, float&, float&, float, float, float, float, float);
+void writePPM(const char*, s_gameData&);
--- makefile
+++ makefile
@@ -1,13 +1,15 @@
+CFLAGS=-O3
+
 all: MyBot
 
 base.o: base.cc base.hh array.hh
-	g++ -c base.cc -o base.o
+	g++ $(CFLAGS) -c base.cc -o base.o
 
 MyBot.o: MyBot.cc
-	g++ -c MyBot.cc -o MyBot.o
+	g++ $(CFLAGS) -c MyBot.cc -o MyBot.o
 
 MyBot: MyBot.o base.o
-	g++ -o MyBot MyBot.o base.o
+	g++ $(CFLAGS) -o MyBot MyBot.o base.o
 
 clean:
 	(rm *.o; rm MyBot;)
